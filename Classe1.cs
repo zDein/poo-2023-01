aa9efea6a68ee66c013221a79fcbef74be141059
Voce <email@gmail.com>
2023-05-29 21:34:42 -0300

    Mudando as operações da classe1 do arquivo principal para um arquivo próprio

--- Classe1.cs
+++ Classe1.cs
@@ -1,12 +1,39 @@
 using System;
 
 namespace Espaco{
-    class Classe1{
-        public string Nome;
-        public short Idade;
+        class Classe1{
+        public string Nome { get; set; }
+        public int NumeroConta { get; set; }
+        public double Saldo { get; private set; }
+
+        public Classe1(){
+            Saldo = 0;
+        }
+
+        public Classe1(string nome, int numeroConta) : this()
+        {
+            this.Nome = nome;
+            this.NumeroConta = numeroConta;
+        }
+
+        public void adicionarSaldo(double valor){
+            Saldo += valor;
+        }
+
+        public void saqueSaldo(double valor){
+            if(Saldo>-1000&&(Saldo-valor)>=-1000){
+                Saldo -= valor;
+            }else{
+                Console.WriteLine("Saldo insuficiente!");
+            }
+        }
 
         public override string ToString(){
-            return "Nome: " + Nome + ", Idade: " + Idade;
+            return "\n---------------------------------------------------\n" 
+            + "Dados do usuário\n" 
+            + "---------------------------------------------------\n" 
+            + "Nome: " + Nome + "\nNumero da conta: " + NumeroConta + "\nSaldo diponível: " + Saldo
+            + "\n---------------------------------------------------\n" ;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -58,39 +58,5 @@ namespace Espaco{
         }
     }
 
-    class Classe1{
-        public string Nome { get; set; }
-        public int NumeroConta { get; set; }
-        public double Saldo { get; private set; }
 
-        public Classe1(){
-            Saldo = 0;
-        }
-
-        public Classe1(string nome, int numeroConta) : this()
-        {
-            this.Nome = nome;
-            this.NumeroConta = numeroConta;
-        }
-
-        public void adicionarSaldo(double valor){
-            Saldo += valor;
-        }
-
-        public void saqueSaldo(double valor){
-            if(Saldo>-1000&&(Saldo-valor)>=-1000){
-                Saldo -= valor;
-            }else{
-                Console.WriteLine("Saldo insuficiente!");
-            }
-        }
-
-        public override string ToString(){
-            return "\n---------------------------------------------------\n" 
-            + "Dados do usuário\n" 
-            + "---------------------------------------------------\n" 
-            + "Nome: " + Nome + "\nNumero da conta: " + NumeroConta + "\nSaldo diponível: " + Saldo
-            + "\n---------------------------------------------------\n" ;
-        }
-    }
 }
